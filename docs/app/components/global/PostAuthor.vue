ebe3cd86f1280c4c85c8691c09902d4b41921251
Anthony Fu <github@antfu.me>
2026-09-16 12:56:11 +0900

    chore: update

--- docs/app/components/global/PostAuthor.vue
+++ docs/app/components/global/PostAuthor.vue
@@ -15,7 +15,6 @@ withDefaults(defineProps<{
     :href="href"
     target="_blank"
     rel="noopener noreferrer"
-    style="margin-top: -5rem;"
     class="not-prose mb-6 inline-flex items-center gap-3 rounded-lg text-inherit no-underline transition-opacity hover:opacity-80"
   >
     <img
